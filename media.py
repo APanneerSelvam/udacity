14b1df408b792d84382ad95ecafb42d8b7d7aa61
unknown <pannadurai@CHE-B8XRFD2.gsiccorp.net>
2018-01-12 15:43:58 +0530

    Formattig the code

--- media.py
+++ media.py
@@ -1,8 +1,10 @@
 import webbrowser
 
+
 class Movie():
     """This movie class will holds the details about the movie"""
-    def __init__(self,movie_title,movie_story,poster_image,youtube_trailer):
+    def __init__(self, movie_title, movie_story,
+                 poster_image, youtube_trailer):
         self.title = movie_title
         self.story_line = movie_story
         self.poster_image_url = poster_image
@@ -10,5 +12,3 @@ class Movie():
 
     def show_trailer(self):
         webbrowser.open(self.trailer_youtube_url)
-        
-        

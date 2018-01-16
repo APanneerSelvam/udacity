af991af10c0d30a93bbe53ef1a1cd0ddf160ef8a
unknown <pannadurai@CHE-B8XRFD2.gsiccorp.net>
2018-01-16 12:13:19 +0530

    Adding documentation for media.py

--- media.py
+++ media.py
@@ -2,13 +2,22 @@ import webbrowser
 
 
 class Movie():
-    """This movie class will holds the details about the movie"""
+    """class describing the attributes of the movie"""
     def __init__(self, movie_title, movie_story,
                  poster_image, youtube_trailer):
+        """Initializes the movie object from the args passed
+
+            Args:
+                movie_title (str): Movie's Title
+                movie_story (str): Story of the movie
+                poster_image (str): URL of the movie poster image
+                youtube_trailer (str): Youtube URL of the movie trailer"""
+        
         self.title = movie_title
         self.story_line = movie_story
         self.poster_image_url = poster_image
         self.trailer_youtube_url = youtube_trailer
 
     def show_trailer(self):
+        """Plays the youtube video on the browser from the trailer URL"""
         webbrowser.open(self.trailer_youtube_url)
